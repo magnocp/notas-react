7feee27135ee2e86c4b8b428e896d81501165599
Magno Costa <magnocp123@gmail.com>
2021-11-29 11:41:49 -0300

    mudança no css

--- src/components/CardNota/index.jsx
+++ src/components/CardNota/index.jsx
@@ -8,17 +8,20 @@ class CardNota extends Component {
   apagar(){
     const indice = this.props.indice
     this.props.apagarNota(indice)
+    console.log(this.props)
   }
 
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota-cabecalho">
-          <h3 className="card-nota-titulo">{this.props.titulo}</h3>
+          <div className="info">
+            <h3 className="card-nota-titulo">{this.props.titulo}</h3>
+            <h4>{this.props.categoria}</h4>
+          </div>
           <DeleteSVG onClick={this.apagar.bind(this)}/>
-          <h4>{this.props.categoria}</h4>
         </header>
-        <p className="card-nota-texto">{this.props.texto}</p>
+        <p className="card-nota-texto">{this.props.text}</p>
       </section>
     );
   }
